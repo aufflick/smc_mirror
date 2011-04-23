cd7114cc0a998610cf8f310bbd7938306986d17d
Francois Perrad <francois.perrad@gadz.org>
2011-04-23 13:13:05 +0000

    clean up CVS change log

--- examples/JavaScript/EX2/AppClass.js
+++ examples/JavaScript/EX2/AppClass.js
@@ -3,20 +3,20 @@
 // License Version 1.1 (the "License"); you may not use this file
 // except in compliance with the License. You may obtain a copy of
 // the License at http://www.mozilla.org/MPL/
-// 
+//
 // Software distributed under the License is distributed on an "AS
 // IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
 // implied. See the License for the specific language governing
 // rights and limitations under the License.
-// 
+//
 // The Original Code is State Machine Compiler (SMC).
-// 
+//
 // The Initial Developer of the Original Code is Charles W. Rapp.
 // Portions created by Charles W. Rapp are
 // Copyright (C) 2000 - 2003 Charles W. Rapp.
 // All Rights Reserved.
-// 
-// Contributor(s): 
+//
+// Contributor(s):
 //
 // Name
 //  AppClass
@@ -30,26 +30,14 @@
 //
 // CHANGE LOG
 // $Log$
+// Revision 1.2  2011/04/23 13:13:06  fperrad
+// clean up CVS change log
+//
 // Revision 1.1  2011/02/16 18:25:10  nitin-nizhawan
 // added second example
 //
 // Revision 1.2  2011/02/16 18:00:30  nitin-nizhawan
 // minor changes
-//
-// Revision 1.1  2011/02/14 18:30:39  nitin-nizhawan
-// Added example 1 for JavaScript
-//
-// Revision 1.6  2009/03/27 09:41:45  cwrapp
-// Added F. Perrad changes back in.
-//
-// Revision 1.5  2009/03/01 18:20:38  cwrapp
-// Preliminary v. 6.0.0 commit.
-//
-// Revision 1.4  2005/05/28 13:51:23  cwrapp
-// Update Java examples 1 - 7.
-//
-// Revision 1.0  2003/12/14 19:50:16  charlesr
-// Initial revision
 //
 
 
