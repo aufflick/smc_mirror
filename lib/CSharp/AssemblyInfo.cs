b513ee684555b50ac2df6b59ffc03c7c4d25eaba
Charles Rapp <rapp@acm.org>
2013-07-14 15:08:11 +0000

    check in for release 6.2.0

--- lib/CSharp/AssemblyInfo.cs
+++ lib/CSharp/AssemblyInfo.cs
@@ -31,6 +31,9 @@
 //
 // CHANGE LOG
 // $Log$
+// Revision 1.5  2013/07/14 15:08:11  cwrapp
+// check in for release 6.2.0
+//
 // Revision 1.4  2009/03/01 18:20:40  cwrapp
 // Preliminary v. 6.0.0 commit.
 //
@@ -47,71 +50,30 @@
 // Initial revision
 //
 
-using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-//
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
 [assembly: AssemblyTitle("SMC statemap .Net Library")]
 [assembly: AssemblyDescription("State Machine Compiler .Net runtime library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("charlesr")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
+[assembly: AssemblyCulture("")]
 
-// Force CLS compliance.
-[assembly: CLSCompliant(true)]
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.0.0")]
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
 
-//
-// In order to sign your assembly you must specify a key to use. Refer to the 
-// Microsoft .NET Framework documentation for more information on assembly signing.
-//
-// Use the attributes below to control which key is used for signing. 
-//
-// Notes: 
-//   (*) If no key is specified, the assembly is not signed.
-//   (*) KeyName refers to a key that has been installed in the Crypto Service
-//       Provider (CSP) on your machine. KeyFile refers to a file which contains
-//       a key.
-//   (*) If the KeyFile and the KeyName values are both specified, the 
-//       following processing occurs:
-//       (1) If the KeyName can be found in the CSP, that key is used.
-//       (2) If the KeyName does not exist and the KeyFile does exist, the key 
-//           in the KeyFile is installed into the CSP and used.
-//   (*) In order to create a KeyFile, you can use the sn.exe (Strong Name) utility.
-//       When specifying the KeyFile, the location of the KeyFile should be
-//       relative to the project output directory which is
-//       %Project Directory%\obj\<configuration>. For example, if your KeyFile is
-//       located in the project directory, you would specify the AssemblyKeyFile 
-//       attribute as [assembly: AssemblyKeyFile("..\\..\\mykey.snk")]
-//   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
-//       documentation for more information on this.
-//
-// [assembly: AssemblyDelaySign(false)]
-// 
-// #if ANT
-// [assembly: AssemblyKeyFile(@"@KeyPath@")]
-// #else
-// [assembly: AssemblyKeyFile(@"..\..\keys.sn")]
-// #endif
-// 
-// [assembly: AssemblyKeyName("")] 
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
